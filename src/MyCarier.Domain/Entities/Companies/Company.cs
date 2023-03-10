70e877cf53c342e39b3e67472f439ea9d714ce77
Temurkhann <temurxonshodiyev4@gmail.com>
2023-03-10 12:37:59 +0500

    DOMAIN: Entities created

---
+++ src/MyCarier.Domain/Entities/Companies/Company.cs
@@ -0,0 +1,15 @@
+﻿using MyCarier.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCarier.Domain.Entities.Companies
+{
+    public class Company : Auditable
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ src/MyCarier.Domain/Entities/Jobs/Job.cs
@@ -0,0 +1,13 @@
+﻿using MyCarier.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCarier.Domain.Entities.Jobs
+{
+    public class Job : Auditable
+    {
+    }
+}
