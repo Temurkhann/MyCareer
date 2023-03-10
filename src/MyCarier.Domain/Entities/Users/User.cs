0e74e237350a58eceb26d084dbead62bed792de1
Temurkhann <temurxonshodiyev4@gmail.com>
2023-03-10 10:32:02 +0500

    Auditable and user entity created

---
+++ src/MyCarier.Domain/Entities/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using MyCarier.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCarier.Domain.Entities.Users
+{
+    public class User : Auditable
+    {
+        public string Email { get; set; }   
+        public string Password { get; set; }
+    }
+}
