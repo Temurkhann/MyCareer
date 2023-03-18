cd11f1160d7464b9b3437d2f37bc86e5a029a943
Temurkhann <temurxonshodiyev4@gmail.com>
2023-03-18 19:27:05 +0500

    SERVICE: Mappers added

---
+++ src/MyCareer.Service/DTOs/Users/UserForViewDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCareer.Service.DTOs.Users
+{
+    public class UserForViewDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public bool IsEmailConfirmed { get; set; }
+    }
+}
---
+++ src/MyCareer.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,120 @@
+﻿using AutoMapper;
+using MyCareer.Domain.Entities.Addresses;
+using MyCareer.Domain.Entities.Attachments;
+using MyCareer.Domain.Entities.Chats;
+using MyCareer.Domain.Entities.Companies;
+using MyCareer.Domain.Entities.Contacts;
+using MyCareer.Domain.Entities.Contracts;
+using MyCareer.Domain.Entities.Educations;
+using MyCareer.Domain.Entities.Experiences;
+using MyCareer.Domain.Entities.Hobbies;
+using MyCareer.Domain.Entities.Languages;
+using MyCareer.Domain.Entities.Likes;
+using MyCareer.Domain.Entities.Resumes;
+using MyCareer.Domain.Entities.Skills;
+using MyCareer.Domain.Entities.Talents;
+using MyCareer.Domain.Entities.Users;
+using MyCareer.Service.DTOs.Addresses;
+using MyCareer.Service.DTOs.Attachments;
+using MyCareer.Service.DTOs.Chats;
+using MyCareer.Service.DTOs.Companies;
+using MyCareer.Service.DTOs.Contacts;
+using MyCareer.Service.DTOs.Contracts;
+using MyCareer.Service.DTOs.Countries;
+using MyCareer.Service.DTOs.Educations;
+using MyCareer.Service.DTOs.Experiences;
+using MyCareer.Service.DTOs.Freelancers;
+using MyCareer.Service.DTOs.Hobbies;
+using MyCareer.Service.DTOs.Jobs;
+using MyCareer.Service.DTOs.Languages;
+using MyCareer.Service.DTOs.Likes;
+using MyCareer.Service.DTOs.Messages;
+using MyCareer.Service.DTOs.Regions;
+using MyCareer.Service.DTOs.Resumes;
+using MyCareer.Service.DTOs.Skills;
+using MyCareer.Service.DTOs.Talents;
+using MyCareer.Service.DTOs.Users;
+using MyCarrier.Domain.Entities.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCareer.Service.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        { 
+            // user
+            CreateMap<User, UserForCreationDTO>().ReverseMap();
+            CreateMap<User, UserForViewDTO>().ReverseMap();
+            CreateMap<UserContact, UserContactForCreationDTO>().ReverseMap();
+            CreateMap<UserHobby, UserHobbyForCreationDTO>().ReverseMap();
+            CreateMap<UserLanguage, UserLanguageForCreationDTO>().ReverseMap();
+
+            // talent
+            CreateMap<Talent, TalentForCreationDTO>().ReverseMap();
+
+            // skill
+            CreateMap<Skill, SkillForCreationDTO>().ReverseMap();
+
+            // resume
+            CreateMap<Resume, ResumeForCreationDTO>().ReverseMap();
+
+            // region
+            CreateMap<Region, RegionForCreationDTO>().ReverseMap();
+
+            // message
+            CreateMap<Message, MessageForCreationDTO>().ReverseMap();
+
+            // like
+            CreateMap<Like, LikeForCreationDTO>().ReverseMap();
+
+            // language
+            CreateMap<Language, LanguageForCreationDTO>().ReverseMap();
+
+            // job
+            CreateMap<JobForCreationDTO, JobForCreationDTO>().ReverseMap();
+            CreateMap<JobSkill, JobSkillForCreationDTO>().ReverseMap();
+
+            // hobby
+            CreateMap<Hobby, HobbyForCreationDTO>().ReverseMap();
+
+            // freelancer
+            CreateMap<Freelancer, FreelancerForCreationDTO>().ReverseMap();
+
+            // experience
+            CreateMap<Experience, ExperienceForCreationDTO>().ReverseMap();
+
+            // education
+            CreateMap<Education, EducationForCreationDTO>().ReverseMap();
+
+            // country
+            CreateMap<Country, CountryForCreationDTO>().ReverseMap();
+
+            // contract
+            CreateMap<Contract, ContractForCreationDTO>().ReverseMap();
+            CreateMap<ContractSkill, ContractSkillForCreationDTO>().ReverseMap();
+            CreateMap<PerformerDetails, PerformerDetailForCreationDTO>().ReverseMap();
+
+            // contact
+            CreateMap<Contact, ContactForCreationDTO>().ReverseMap();
+
+            // company
+            CreateMap<Company, CompanyForCreationDTO>().ReverseMap();
+            CreateMap<CompanyInformation, CompanyInformationForCreationDTO>().ReverseMap();
+
+            //chat
+            CreateMap<Chat, ChatForCreationDTO>().ReverseMap();
+
+            // attachment
+            CreateMap<Attachment, AttachmentForCreationDTO>().ReverseMap();
+
+            // address
+            CreateMap<Address, AddressForCreationDTO>().ReverseMap();
+        }
+            
+    }
+}
