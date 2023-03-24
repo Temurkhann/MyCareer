d4938057d7abb0827e98c83529430b93497c7190
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-03-24 14:09:53 +0500

    SERVICE: LanguageService and LikeService completed

--- src/MyCareer.Service/Interfaces/Chats/IMessageService.cs
+++ src/MyCareer.Service/Interfaces/Chats/IMessageService.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace MyCareer.Service.Interfaces.IChats
+namespace MyCareer.Service.Interfaces.Messages
 {
     public interface IMessageService
     {
@@ -14,6 +14,6 @@ namespace MyCareer.Service.Interfaces.IChats
         ValueTask<Message> GetAsync(Expression<Func<Message, bool>> expression);
         ValueTask<Message> CreateAsync(MessageForCreationDTO messageForCreationDTO);
         ValueTask<bool> DeleteAsync(int id);
-        ValueTask<Message> Update(int id, MessageForCreationDTO messageForCreationDTO);
+        ValueTask<Message> Update(int id, MessageForCreationDTO messageForCreation);
     }
 }
--- src/MyCareer.Service/Services/Users/UserSkillService.cs
+++ src/MyCareer.Service/Services/Users/UserSkillService.cs
@@ -10,7 +10,6 @@ using MyCareer.Service.Exceptions;
 using MyCareer.Service.Interfaces.IUsers;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
