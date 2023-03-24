bb3eea87c0d1fd2d3edd1425bc812f7bc679ab8d
Temurkhann <temurxonshodiyev4@gmail.com>
2023-03-24 18:10:22 +0500

    APPLICATION: Some services registered and ChangePassword added

--- src/MyCareer.Api/Extensions/ServiceExtensions.cs
+++ src/MyCareer.Api/Extensions/ServiceExtensions.cs
@@ -3,8 +3,29 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MyCareer.Data.IRepositories;
 using MyCareer.Data.Repositories;
+using MyCareer.Domain.Entities.Companies;
+using MyCareer.Domain.Entities.Languages;
+using MyCareer.Domain.Entities.Likes;
+using MyCareer.Domain.Entities.Resumes;
+using MyCareer.Domain.Entities.Skills;
+using MyCareer.Domain.Entities.Talents;
 using MyCareer.Domain.Entities.Users;
+using MyCareer.Service.Interfaces.Companies;
+using MyCareer.Service.Interfaces.Freelancers;
+using MyCareer.Service.Interfaces.IUsers;
+using MyCareer.Service.Interfaces.Languages;
+using MyCareer.Service.Interfaces.Likes;
+using MyCareer.Service.Interfaces.Resumes;
+using MyCareer.Service.Interfaces.Skills;
+using MyCareer.Service.Interfaces.Talents;
 using MyCareer.Service.Interfaces.Users;
+using MyCareer.Service.Services.Companies;
+using MyCareer.Service.Services.Freelancers;
+using MyCareer.Service.Services.Languages;
+using MyCareer.Service.Services.Likes;
+using MyCareer.Service.Services.Resumes;
+using MyCareer.Service.Services.Skills;
+using MyCareer.Service.Services.Talents;
 using MyCareer.Service.Services.Users;
 using System.Text;
 
@@ -17,9 +38,30 @@ namespace ZaminEducation.Api
         {
             // repositories
             services.AddScoped<IGenericRepository<User>, GenericRepository<User>>();
+            services.AddScoped<IGenericRepository<UserHobby>, GenericRepository<UserHobby>>();
+            services.AddScoped<IGenericRepository<UserSkill>, GenericRepository<UserSkill>>();
+            services.AddScoped<IGenericRepository<UserLanguage>, GenericRepository<UserLanguage>>();
+            services.AddScoped<IGenericRepository<Talent>, GenericRepository<Talent>>();
+            services.AddScoped<IGenericRepository<Skill>, GenericRepository<Skill>>();
+            services.AddScoped<IGenericRepository<Resume>, GenericRepository<Resume>>();
+            services.AddScoped<IGenericRepository<Like>, GenericRepository<Like>>();
+            services.AddScoped<IGenericRepository<Language>, GenericRepository<Language>>();
+            services.AddScoped<IGenericRepository<Freelancer>, GenericRepository<Freelancer>>();
+            services.AddScoped<IGenericRepository<Company>, GenericRepository<Company>>();
 
             // services
             services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IUserSkillService, UserSkillService>();
+            services.AddScoped<IUserHobbyService, UserHobbyService>();
+            services.AddScoped<IUserSkillService, UserSkillService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITalentService, TalentService>();
+            services.AddScoped<ISkillService, SkillService>();
+            services.AddScoped<IResumeService, ResumeService>();
+            services.AddScoped<ILikeService, LikeService>();
+            services.AddScoped<ILanguageService, LanguageService>();
+            services.AddScoped<IFreelancerService, FreelancerService>();
+            services.AddScoped<ICompanyService, CompanyService>();
         }
 
         public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
--- src/MyCareer.Service/Services/Users/UserService.cs
+++ src/MyCareer.Service/Services/Users/UserService.cs
@@ -5,6 +5,7 @@ using MyCareer.Domain.Configurations;
 using MyCareer.Domain.Entities.Users;
 using MyCareer.Service.DTOs.Users;
 using MyCareer.Service.Exceptions;
+using MyCareer.Service.Extensions;
 using MyCareer.Service.Helpers;
 using MyCareer.Service.Interfaces.Users;
 using System;
@@ -24,6 +25,25 @@ namespace MyCareer.Service.Services.Users
             this.userRepository = userRepository;
             this.mapper = mapper;
         }
+
+        public async ValueTask<bool> ChangePasswordAsync(UserForChangePasswordDTO userForChangePasswordDTO)
+        {
+            var user = await userRepository.GetAsync(u => u.Id == HttpContextHelper.UserId);
+
+            if (user == null)
+                throw new MyCareerException(404, "User not found");
+
+            if (user.Password != userForChangePasswordDTO.OldPassword.Encrypt())
+                throw new MyCareerException(400, "Password is incorrect");
+
+
+            user.Password = userForChangePasswordDTO.NewPassword.Encrypt();
+
+            userRepository.Update(user);
+            await userRepository.SaveChangesAsync();
+            return true;
+        }
+
         public async ValueTask<UserForViewDTO> CreateAsync(UserForCreationDTO userForCreationDTO)
         {
             var existEmail = await userRepository.GetAsync(u => u.Email == userForCreationDTO.Email);
