48a44f19319fbbed07e6bd688686eafc4d437cc5
Temurkhann <temurxonshodiyev4@gmail.com>
2023-03-25 14:55:38 +0500

    APPLICATION: Attachment controller created

---
+++ src/MyCareer.Api/Extensions/FormFileExtensions.cs
@@ -0,0 +1,26 @@
+﻿using MyCareer.Service.DTOs.Attachments;
+
+namespace MyCareer.Api.Extensions
+{
+    public static class FormFileExtensions
+    {
+        public static AttachmentForCreationDTO ToAttachmentOrDefault(this IFormFile formFile)
+        {
+
+            if (formFile?.Length > 0)
+            {
+                using var ms = new MemoryStream();
+                formFile.CopyTo(ms);
+                var fileBytes = ms.ToArray();
+
+                return new AttachmentForCreationDTO()
+                {
+                    Name = formFile.FileName,
+                    Stream = new MemoryStream(fileBytes)
+                };
+            }
+
+            return null;
+        }
+    }
+}
