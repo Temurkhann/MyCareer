7b01a3e9c600583d810f927ef028241a42ee91d2
Temurkhann <temurxonshodiyev4@gmail.com>
2023-03-29 13:13:38 +0500

    APPLICATION: ChatController and MessageController created

---
+++ src/MyCareer.Api/Controllers/Chats/ChatController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using MyCareer.Domain.Configurations;
+using MyCareer.Service.DTOs.Chats;
+using MyCareer.Service.Interfaces.Chats;
+using System.Threading.Tasks;
+
+namespace MyCareer.Api.Controllers.Chats
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> hubContext;
+        private readonly IChatService chatService;
+
+        public ChatController(IHubContext<ChatHub> hubContext, IChatService chatService)
+        {
+            this.hubContext = hubContext;
+            this.chatService = chatService;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(string user, string message)
+        {
+            await hubContext.Clients.All.SendAsync("RecieveMessage", user, message);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+            => Ok(await chatService.GetAll(@params));
+
+        [HttpGet("{id}")]
+        public async ValueTask<IActionResult> GetAsync([FromRoute] int id)
+            => Ok(await chatService.GetAsync(u => u.Id == id));
+    }
+}
