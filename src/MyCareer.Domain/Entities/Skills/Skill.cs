417e3377d7ac7780dd9d1728da795ffb753800a6
Temurkhann <temurxonshodiyev4@gmail.com>
2023-04-06 22:35:45 +0500

    APPLICATION: ContactUsController created

--- src/MyCareer.Domain/Entities/Skills/Skill.cs
+++ src/MyCareer.Domain/Entities/Skills/Skill.cs
@@ -1,12 +1,10 @@
 ﻿using MyCareer.Domain.Commons;
 using MyCareer.Domain.Enums;
-using System.ComponentModel.DataAnnotations;
 
 namespace MyCareer.Domain.Entities.Skills
 {
     public class Skill : Auditable
     {
-        [MaxLength(64)]
         public string Content { get; set; }
         public Position Position { get; set; }
     }
